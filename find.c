a014c1931558b68cd87978dca6277b97d8e22c6b
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2024-08-19 16:29:51 -0400

    Initial commit

---
+++ find.c
@@ -0,0 +1,24 @@
+/*Programa: Projeto FIND.
+Descrição: O programa será capaz de buscar um arquivo passado como argumento por qualquer pasta passada como argumento via terminal de comando
+Programador: Hugo Josué Lema Das Neves 
+RGM: 47159 
+Data da última modificação: 19/08/2024 
+
+
+Instruções para compilação
+gcc find.c -o find.exe funcoes.c -Wall -pedantic -Wextra -Werror
+
+Instruções para execução
+find.exe nome_da_pasta nome_do_arquivo
+*/
+
+# include <stdio.h>
+# include "funcoes.h"
+
+int main(int argc, char *argv[])
+{
+    argc = argc;
+    argv = argv;
+
+    return 0;
+}
